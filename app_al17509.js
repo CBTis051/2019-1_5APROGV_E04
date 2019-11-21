3da8cd528974bb28a9ea17b0932f15abe9a4c984
Johana Monserrat Nieto <al17509@cbtis051.edu.mx>
2019-11-21 16:29:02 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17509.js
+++ app_al17509.js
@@ -1,7 +1,11 @@
 var http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-
-res.end('Hola mundo Node JS - Nieto Johana Monserrat\n');
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT- Nieto Johana Monserrat\n'
+    };
+    var json = JSON.stringify(objeto);
+    
+    res.end(json);
 }).listen(8081);
 console.log('Server running on port 8081');
