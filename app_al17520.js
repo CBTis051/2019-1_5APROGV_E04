4980d96fc24dee873a93a94d3a9f323875386811
Ricardo Isaac Rivera Sánchez <al17520@cbtis051.edu.mx>
2019-11-22 10:43:36 -0700

    Modificación de error en el codigo

--- app_al17520.js
+++ app_al17520.js
@@ -3,14 +3,13 @@ http.createServer(function (req, res)
 {
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
-    var objeto = 
-    {
+    var objeto = {
         mensaje : 'Hola mundo Node JS Rep GIT - Rivera Sánchez Ricardo Isaac \n'
     };
 
     var json = JSON.stringify(objeto);
 
-    res.end(json)
+    res.end(json);
 
 }).listen(8081);
 console.log('Server Runing on Port 8081');
