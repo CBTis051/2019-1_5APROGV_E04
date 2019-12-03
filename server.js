f8031aaecafc4a8d387dc8afc46ed68c73cb9359
Ricardo Isaac Rivera Sánchez <al17520@cbtis051.edu.mx>
2019-12-03 11:31:17 -0700

    cambios en server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 require('./config/config');
 
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
+
+const bosyparser = require('body-parser');
+// parse aplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded( { extended: false}) )
+
+// parse aplication/json
+app.use(bodyParser.json())
 
 /**
  * Home server
