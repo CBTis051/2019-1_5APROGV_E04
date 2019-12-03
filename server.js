b01f669e4c641817a59d6a2edbf23fceb025a1a2
Ricardo Isaac Rivera Sánchez <al17520@cbtis051.edu.mx>
2019-12-02 20:52:24 -0700

    Agregamos el Home o EndPoint

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+
+/**
+ * Home server
+ */
+app.get('/', (req, res) => {
+    let objeto = {
+        mensaje : "Bienvenido al REST server de 5APROGV Equipo 04"
+    };
+
+    res.send(objeto);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on Port ", process.env.PORT);
+});
